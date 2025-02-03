1cd8b7d9cc7fc3a0be0f02cc344fd46078fecd45
zhaosw <hello@tineaine.cn>
2025-02-03 19:29:08 +0800

    build(dependencies): 更新依赖并配置 Element Plus
    - 移除 main.ts 中的 ElementPlus 导入语句
    - 在 package.json 中添加 @icon-park/vue-next 和 rollup 依赖
    - 更新 vite.config.ts，添加 AutoImport 和 Components 插件
    - 新增 auto-imports.d.ts 和 components.d.ts 文件，用于自动导入和组件声明

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,36 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 
 // @ts-expect-error process is a Node.js global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),],
+    clearScreen: false,
+    server: {
+        port: 1420,
+        strictPort: true,
+        host: host || false,
+        hmr: host
+            ? {
+                protocol: "ws",
+                host,
+                port: 1421,
+            }
+            : undefined,
+        watch: {
+            // 不监听 src-tauri 目录下的文件
+            ignored: ["**/src-tauri/**"],
+        },
     },
-  },
 }));
